558f5e8fb865c2cc96ae098c890b000f6d707f35
Humphrey Boahen <humphreyboahen432@gmail.com>
2024-01-24 10:42:40 -0600

    1.5

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -3,27 +3,43 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
+
+        // Creating a scanner to read user input
         Scanner scanner = new Scanner(System.in);
 
+        // Run the program run while the user wants to
         while (true) {
+            // Declaring a list of possible moves for the computer
             String[] rps = {"r", "p", "s"};
+
+            // Generating a random move for the computer
             String computerMove = rps[new Random().nextInt(rps.length)];
 
-            
+            // Declaring a variable to store the player's move
             String playerMove;
 
+            // Validation loop to check the player's input
             while (true){
+                // Prompting the player to enter their move
                 System.out.println("Please enter your move (r, p ,s)");
+
+                // Storing the player's move
                 playerMove = scanner.nextLine();
 
+
+                // Checking if the player's move is valid and breaking out of the loop
                 if (playerMove.equals("r") || playerMove.equals("s") || playerMove.equals("p")) {
                     break;                
                 }
+
+                // If the player's move is not valid, print an error message
                 System.out.println(playerMove + " is not a valid move.");
             }
 
+            // Printing the computer's move
             System.out.println("Computer played: " + computerMove);
 
+            // Determining the winner of the game
             if (playerMove.equals(computerMove)) {
                 System.out.println("Game is a tie");
             }
@@ -51,13 +67,18 @@ public class RockPaperScissors {
                     System.out.println("Yah, you win!");
                 }
             }
+
+            // Prompting the player if they want to play again
             System.out.println("Do you want to play again? (y/n)");
             String playAgain = scanner.nextLine();
             
+            // Checking if the player wants to play again
             if (!playAgain.equals("y")) {
                 break;
             }
         }
+
+        // Closing the scanner
         scanner.close();
     
     }
